4b7c0cf8e7f0d5d7670cdf63136ebcee98a2c030
Bergrebell <rrkuepper@gmail.com>
2017-04-06 14:09:59 +0200

    adds basic user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 
 Restaurant.create(name: "Lilly Jo", current_votes: 0)
 Restaurant.create(name: "New Point", current_votes: 0)
+
+User.create(name: "Roman", email: "roman.kuepper@simplificator.com", password: "Password123!")
